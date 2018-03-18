ea75fb46b15acf7af9ac5496d327125e6bf16348
Kacpro <kacperj97@gmail.com>
2018-03-18 20:40:02 +0100

    Added SysOpy lab2

---
+++ SysOpy/lab2/src/zad1/zad1.c
@@ -0,0 +1,232 @@
+#include <stdlib.h>
+#include <bits/types/FILE.h>
+#include <stdio.h>
+#include <memory.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/resource.h>
+
+
+int parse(int argc, char** argv);
+
+void generate(char* fileName, int numberOfRecords, int recordSize);
+
+void sort(char* fileName, int numberOfRecords, int recordSize, char* mode);
+
+void copy(char* sourceName, char* destinationName, int howManyRecords, int bufferSize, char* mode);
+
+
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+    parse(argc, argv);
+
+    return 0;
+}
+
+
+void showResults(struct rusage start, struct rusage end, char* operation)
+{
+    long double timeU = (end.ru_utime.tv_sec + (end.ru_utime.tv_usec * 1.0)/1e6 - start.ru_utime.tv_sec - (start.ru_utime.tv_usec * 1.0)/1e6);
+    long double timeS = (end.ru_stime.tv_sec + (end.ru_stime.tv_usec * 1.0)/1e6 - start.ru_stime.tv_sec - (start.ru_stime.tv_usec * 1.0)/1e6);
+    printf("\nUser time:\t%Lf\nSystem time:\t%Lf\n\n", timeU, timeS);
+}
+
+int parse(int argc, char** argv)
+{
+    struct rusage start, end;
+
+    if (!strcmp(argv[1], "generate"))
+    {
+        if (argc != 5) return -1;
+        char* fileName = argv[2];
+        int numberOfRecords = atoi(argv[3]);
+        int recordSize = atoi(argv[4]);
+        generate(fileName, numberOfRecords, recordSize);
+        return 0;
+    }
+    else if (!strcmp(argv[1], "sort"))
+    {
+        if (argc != 6) return -1;
+        if (strcmp(argv[5], "sys") && strcmp(argv[5], "lib")) return -1;
+        char* filePath = argv[2];
+        int numberOfRecords = atoi(argv[3]);
+        int recordSize = atoi(argv[4]);
+        char* mode = argv[5];
+
+        getrusage(RUSAGE_SELF, &start);
+        sort(filePath, numberOfRecords, recordSize, mode);
+        getrusage(RUSAGE_SELF, &end);
+        showResults(start, end, "Sort");
+
+        return 0;
+    }
+    else if (!strcmp(argv[1], "copy"))
+    {
+        if (argc != 7) return -1;
+        if (strcmp(argv[6], "sys") && strcmp(argv[6], "lib")) return -1;
+        char* sourcePath = argv[2];
+        char* destinationName = argv[3];
+	int howManyRecords = atoi(argv[4]);
+        int bufferSize = atoi(argv[5]);
+        char* mode = argv[6];
+
+        getrusage(RUSAGE_SELF, &start);
+        copy(sourcePath, destinationName, howManyRecords, bufferSize, mode);
+        getrusage(RUSAGE_SELF, &end);
+        showResults(start, end, "Copy");
+
+        return 0;
+    }
+    else
+    {
+        printf("Error");
+        return -1;
+    }
+}
+
+
+unsigned char* generateRandomString(int length)
+{
+    unsigned char* result = calloc(length, sizeof(char));
+    for (int i=0; i<length; i++)
+    {
+        result[i] = (unsigned char) (rand() % (256));
+    }
+    return result;
+}
+
+
+void generate(char* fileName, int numberOfRecords, int recordSize)
+{
+    FILE* handler = fopen(fileName, "w");
+    for (int i=0; i<numberOfRecords; i++)
+    {
+        unsigned char* record = generateRandomString(recordSize);
+        fwrite(record, sizeof(unsigned char), recordSize, handler); //dodać sprawdzanie czy się zapisało
+    }
+    fclose(handler);
+}
+
+
+
+void sort(char *fileName, int numberOfRecords, int recordSize, char *mode)
+{
+    unsigned char* buffer1 = calloc(recordSize, sizeof(char));
+    unsigned char* buffer2 = calloc(recordSize, sizeof(char));
+
+    if (!strcmp(mode, "lib"))
+    {
+        FILE* handler = fopen(fileName, "r+");
+
+        for (int i=1; i<numberOfRecords; i++)
+        {
+            fseek(handler, recordSize*i, 0);
+	    fread(buffer1, sizeof(unsigned char), recordSize, handler);
+
+	    int j = i-1;
+	    do
+	    {
+		fseek(handler, recordSize*j, 0);
+		fread(buffer2, sizeof(unsigned char), recordSize, handler);
+		if (buffer1[0] < buffer2[0])
+		{
+			printf("%#o  %#o  %d\n", buffer1[0], buffer2[0], j);
+			fseek(handler, recordSize*(j+1),0);
+			fwrite(buffer2, sizeof(unsigned char), recordSize, handler);
+		}
+		j--;
+	    }
+	    while (buffer1[0] < buffer2[0] && j>=0);
+	    
+	    if (buffer1[0] >= buffer2[0]) fseek(handler, recordSize*(j+2), 0);
+	    else fseek(handler, recordSize*(j+1), 0);
+
+	    fwrite(buffer1, sizeof(unsigned char), recordSize, handler);
+        }
+
+        fclose(handler);
+    }
+    else
+    {
+        int handler = open(fileName, O_RDWR);
+
+        for (int i=1; i<numberOfRecords; i++)
+        {
+                lseek(handler, recordSize * i, SEEK_SET);
+                read(handler, buffer1, recordSize);
+
+	    int j = i-1;
+	    do
+	    {
+		lseek(handler, recordSize*j, SEEK_SET);
+		read(handler, buffer2, recordSize);
+		if (buffer1[0] < buffer2[0])
+		{
+		    lseek(handler, recordSize * (j+1), SEEK_SET);
+	            write(handler, buffer2, recordSize);
+		}
+		j--;
+	    }
+	    while (buffer1[0] < buffer2[0] && j>=0);
+	    
+	    if (buffer1[0] >= buffer2[0]) lseek(handler, recordSize*(j+2), SEEK_SET);
+	    else lseek(handler, recordSize*(j+1), SEEK_SET);
+
+	    write(handler, buffer1, recordSize);		
+
+        }
+        close(handler);
+    }
+
+    free(buffer1);
+    free(buffer2);
+}
+
+
+
+void copy(char *sourceName, char *destinationName, int howManyRecords, int bufferSize, char *mode)
+{
+    if (!strcmp(mode, "lib"))
+    {
+        FILE* handler1 = fopen(sourceName, "r");
+        FILE* handler2 = fopen(destinationName, "w");
+
+        char* buffer = calloc(bufferSize, sizeof(char));
+	
+	int recNum = howManyRecords;
+        int buf;
+        while((buf = fread(buffer, sizeof(char), bufferSize, handler1)) && recNum > 0)
+        {
+            fwrite(buffer, sizeof(char), buf, handler2);
+	    recNum--;
+        }
+
+        fclose(handler1);
+        fclose(handler2);
+        free(buffer);
+    }
+    else
+    {
+        int handler1 = open(sourceName, O_RDONLY);
+        int handler2 = open(destinationName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+        char* buffer = calloc(bufferSize, sizeof(char));
+
+        int buf;
+	int recNum = howManyRecords;
+        while((buf=read(handler1, buffer, sizeof(char))) && recNum > 0)
+        {
+            write(handler2, buffer, buf);
+	    recNum--;
+        }
+
+        close(handler1);
+        close(handler2);
+        free(buffer);
+    }
+}
+
+
---
+++ SysOpy/lab2/src/zad2/zad2.c
@@ -0,0 +1,225 @@
+#define _XOPEN_SOURCE 500
+#include<dirent.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include <time.h>
+#include <ftw.h>
+#include <printf.h>
+
+
+void dirInfo_stat(char* filePath);
+int parse(int argc, char** argv);
+void dirInfo_nftw(char* filePath);
+
+
+char* date;
+char* operator;
+
+
+
+
+void printHelp()
+{
+    printf("Required arguments:\n\t- directory path\n\t- operator (< > =)\n\t- date (yyyy-mm-dd)\n\t- mode (0 1)");
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+    if (parse(argc, argv) != 0)
+        printHelp();
+    return 0;
+}
+
+
+
+
+int parse(int argc, char** argv)
+{
+    if (argc != 5) return -1;
+    if (!strcmp(argv[2], "=") && !strcmp(argv[2], "<") && !strcmp(argv[2], ">")) return -1;
+    char* filePath = argv[1];
+    operator = argv[2];
+    date = argv[3];
+    int mode = atoi(argv[4]);
+    if (mode != 1 && mode != 0) return -1;
+
+    if (mode == 0) dirInfo_stat(filePath);
+    else dirInfo_nftw(filePath);
+
+    return 0;
+}
+
+
+
+
+time_t stringToDate(char *date)
+{
+    int year = 0, month = 0, day = 0;
+
+    time_t argDate = 0;
+    if (sscanf(date, "%4d-%2d-%2d", &year, &month, &day) == 3)
+    {
+        struct tm argTimeStruct = {0};
+        argTimeStruct.tm_year = year;
+        argTimeStruct.tm_mon = month;
+        argTimeStruct.tm_mday = day;
+        argDate = mktime(&argTimeStruct);
+    }
+    return argDate;
+}
+
+
+
+
+char* dateToString(time_t date)
+{
+    char* timeStr = calloc(20, sizeof(char));
+    struct tm* timeinfo = localtime(&date);
+    strftime(timeStr, 20, "%F", timeinfo);
+    return timeStr;
+}
+
+
+
+
+void printInfo(const char* path, const struct stat* stats)
+{
+    char* sysDate = dateToString((stats->st_mtime));
+
+    printf( (S_ISDIR(stats->st_mode)) ? "d" : "-");
+    printf( (stats->st_mode & S_IRUSR) ? "r" : "-");
+    printf( (stats->st_mode & S_IWUSR) ? "w" : "-");
+    printf( (stats->st_mode & S_IXUSR) ? "x" : "-");
+    printf( (stats->st_mode & S_IRGRP) ? "r" : "-");
+    printf( (stats->st_mode & S_IWGRP) ? "w" : "-");
+    printf( (stats->st_mode & S_IXGRP) ? "x" : "-");
+    printf( (stats->st_mode & S_IROTH) ? "r" : "-");
+    printf( (stats->st_mode & S_IWOTH) ? "w" : "-");
+    printf( (stats->st_mode & S_IXOTH) ? "x" : "-");
+
+    printf("\t%li",stats->st_size);
+    printf("\t%s", sysDate);
+    printf("\t%s\n", path);
+}
+
+
+
+
+int fn(const char* fullPath, const struct stat* stats, int flagType, struct FTW* ftwBuf)
+{
+    if (flagType == FTW_F)
+    {
+        time_t argDate = stringToDate(date);
+        time_t systemDate = stringToDate(dateToString(stats->st_mtime));
+
+        if (!strcmp(operator, "="))
+        {
+            if (difftime(argDate, systemDate) == 0)
+            {
+                printInfo(fullPath, stats);
+            }
+        }
+        else if (!strcmp(operator, ">"))
+        {
+            if (difftime(argDate, systemDate) < 0)
+            {
+                printInfo(fullPath, stats);
+            }
+        }
+        else if (!strcmp(operator, "<"))
+        {
+            if (difftime(argDate, systemDate) > 0)
+            {
+                printInfo(fullPath, stats);
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
+void dirInfo_nftw(char* filePath)
+{
+    char path[1000];
+    nftw(realpath(filePath, path), fn, 1, FTW_PHYS);
+}
+
+
+
+
+void dirInfo_stat(char* filePath)
+{
+    DIR* directory = opendir(filePath);
+    if (directory == NULL)
+    {
+        printf("Directory error");
+        exit(-1);
+    }
+
+    struct dirent* fileIterator = readdir(directory);
+    struct stat stats;
+
+    while (fileIterator != NULL)
+    {
+        if (strcmp(fileIterator->d_name,".")==0 || strcmp(fileIterator->d_name,"..")==0) {fileIterator = readdir(directory); continue; }
+
+        char buffer[1000];
+        strcpy(buffer,filePath);
+        strcat(buffer,"/");
+        strcat(buffer,fileIterator->d_name);
+        lstat(buffer, &stats);
+
+	char path[1000];
+
+        if((S_ISDIR(stats.st_mode)))
+        {
+            dirInfo_stat(realpath(buffer, path));
+        }
+
+        if(!(S_ISREG(stats.st_mode)))
+        {
+            fileIterator = readdir(directory);
+            continue;
+        }
+
+        time_t argDate = stringToDate(date);
+        time_t systemDate = stringToDate(dateToString(stats.st_mtime));
+
+        if (!strcmp(operator, "="))
+        {
+            if (difftime(argDate, systemDate) != 0)
+            {
+                fileIterator = readdir(directory);
+                continue;
+            }
+        }
+        else if (!strcmp(operator, ">"))
+        {
+            if (difftime(argDate, systemDate) >= 0)
+            {
+                fileIterator = readdir(directory);
+                continue;
+            }
+        }
+        else if (!strcmp(operator, "<"))
+        {
+            if (difftime(argDate, systemDate) <= 0)
+            {
+                fileIterator = readdir(directory);
+                continue;
+            }
+        }
+
+        printInfo(realpath(buffer, path), &stats);
+
+        fileIterator = readdir(directory);
+    }
+    closedir(directory);
+}
