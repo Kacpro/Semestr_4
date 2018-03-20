ef2cc1b7b36f67576154025788afde8c9a7c1c89
Kacpro <kacperj97@gmail.com>
2018-03-20 17:07:18 +0100

    SysOpy lab3 zad2

---
+++ SysOpy/lab3/src/zad2.c
@@ -0,0 +1,100 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+void readAndExecute(char* fileName)
+{
+    FILE* file = fopen(fileName, "r");
+    if (file == NULL)
+    {
+        printf("File opening error");
+        exit(-1);
+    }
+
+    size_t bufSize = 100;
+    char* buffer = calloc(bufSize, sizeof(char));
+
+    while (getline(&buffer, &bufSize, file) != -1)
+    {
+
+        char* bufferCopy = calloc(bufSize, sizeof(char));
+        strcpy(bufferCopy, buffer);
+
+        char** arguments = NULL;
+        char* p = strtok(bufferCopy, " ");
+        int n_spaces = 0;
+
+        while(p)
+        {
+            arguments = realloc(arguments, sizeof(char*)* ++n_spaces);
+            if (arguments == NULL) exit(-1);
+
+            arguments[n_spaces-1] = p;
+            p = strtok(NULL, " ");
+        }
+
+        arguments = realloc(arguments, sizeof(char*) * (n_spaces + 1));
+        arguments[n_spaces] = 0;
+
+        for (int i=0; i<=n_spaces; i++)
+            strtok(arguments[i], "\n");
+
+        int result = 0;
+        pid_t child = vfork();
+
+        if (child < 0) exit(-1);
+        else if (child == 0)
+        {
+            if (execvp(arguments[0], arguments) == -1)
+            {
+                if (execv(basename(arguments[0]), arguments) == -1)
+                {
+                    exit(-1);
+                }
+            }
+            exit(0);
+        }
+        else
+        {
+            wait(&result);
+            if (WEXITSTATUS(result) != 0)
+            {
+                printf("!!!Operation terminated!!!");
+                exit(-1);
+            }
+        }
+
+        free(bufferCopy);
+        free(arguments);
+    }
+
+    free(buffer);
+    fclose(file);
+}
+
+
+int parse(int argc, char** argv)
+{
+    if (argc != 2) return -1;
+    char* fileName = argv[1];
+    readAndExecute(fileName);
+    return 0;
+}
+
+
+void printHelp()
+{
+    printf("Error");
+}
+
+
+int main(int argc, char** argv)
+{
+    if (parse(argc, argv) != 0) printHelp();
+    return 0;
+}
