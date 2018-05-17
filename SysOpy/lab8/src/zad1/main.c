b08fd8e91b3d9a47151588f79137b5fd2e116462
Kacpro <kacperj97@gmail.com>
2018-05-17 17:54:54 +0200

    SysOpy lab8 zad1

--- SysOpy/lab8/src/zad1/main.c
+++ SysOpy/lab8/src/zad1/main.c
@@ -14,6 +14,7 @@ int WIDTH;
 int HEIGHT;
 int NUM_OF_THREADS;
 int FILTER_SIZE;
+int* FINISHED;
 
 
 int** parseImage(char* fileName)
@@ -48,6 +49,9 @@ int** parseImage(char* fileName)
             i++;
         }
     }
+
+    free(line);
+
     return result;
 }
 
@@ -70,6 +74,7 @@ double** parseFilter(char* fileName)
     while(fgets(line, 128, file))
     {
         sscanf(strtok(line, " \n"), "%lf", &result[i/FILTER_SIZE][i%FILTER_SIZE]);
+
         i++;
         char* buf;
         while((buf = strtok(NULL, " \n")) != NULL)
@@ -77,54 +82,44 @@ double** parseFilter(char* fileName)
             sscanf(buf, "%lf", &result[i/FILTER_SIZE][i%FILTER_SIZE]);
             i++;
         }
+
     }
+
+    free(line);
+
     return result;
 }
 
 
 void* calculate(void* segment)
 {
-    perror("calc");
     int c = ceil(FILTER_SIZE*1.0/2);
-    printf("seg %d\n", *(int*)segment);
-    printf("num %d\n", NUM_OF_THREADS);
-    printf("wid %d\n", WIDTH);
-    perror("calc start");
-
+    int maxWidth = (*(int*)segment + 1 != NUM_OF_THREADS)?(((*(int*)segment)+1) * WIDTH/NUM_OF_THREADS):WIDTH;
 
     for (int x=1; x<=HEIGHT; x++)
     {
-        int maxWidth = (*(int*)segment + 1 != NUM_OF_THREADS)?(*(int*)(segment+1) * WIDTH/NUM_OF_THREADS):WIDTH;
-        printf("max %d\n", maxWidth);
         for (int y=(*(int*)(segment) * WIDTH/NUM_OF_THREADS)+1; y<=maxWidth; y++)
         {
-            perror(NULL);
             double result = 0;
             for (int i=1; i<=FILTER_SIZE; i++)
             {
-                perror("a");
                 for (int j=1; j<=FILTER_SIZE; j++)
                 {
-                    perror("b");
-                    printf("i = %d  j = %d  x = %d  y = %d\n", i, j, x, y);
                     result  += (((1>=x-c+i)?1:(x-c+i))-1 < HEIGHT && ((1>=y-c+j)?1:(y-c+j))-1 < WIDTH)?INPUT[((1>=x-c+i)?1:(x-c+i))-1][((1>=y-c+j)?1:(y-c+j))-1]*FILTER[i-1][j-1]:0;
-                    perror("c");
                 }
             }
-            perror("d");
-            printf("%d  %d\n", x, y);
             OUTPUT[x-1][y-1] = round(result);
-            perror("e");
         }
     }
-    perror("calc end");
+
+    FINISHED[*(int*)segment] = 1;
+
     return 0;
 }
 
 
-void lifeOfThread()
+void doSomeWork()
 {
-    perror("life");
     OUTPUT = calloc(HEIGHT, sizeof(int*));
     for (int i=0; i<HEIGHT; i++)
         OUTPUT[i] = calloc(WIDTH, sizeof(int));
@@ -132,29 +127,53 @@ void lifeOfThread()
     pthread_t* threads = calloc(NUM_OF_THREADS, sizeof(pthread_t));
 
     long start, end;
-
     start = times(NULL);
-perror("life mid");
 
-
-
-// tablica na inty do wskaznikow TODO
-    // i nie można przekazywać przez referencję bo się zmienia i wyjebuje
+    int** ints = calloc(NUM_OF_THREADS, sizeof(int*));
     for (int i=0; i<NUM_OF_THREADS; i++)
     {
-        perror("st");
-        void* ptr = &i;
-        printf("ptr %d\n", *(int*)ptr);
-        pthread_create(&threads[i], NULL, calculate, ptr);
-        perror(NULL);
-        sleep(5);
+        ints[i] = calloc(1, sizeof(int));
+        *ints[i] = i;
     }
 
-    perror("life after");
+    for (int i=0; i<NUM_OF_THREADS; i++)
+    {
+        pthread_create(&threads[i], NULL, calculate, ints[i]);
+    }
+
+    while(1)
+    {
+        int flag = 1;
+        for (int i=0; i<NUM_OF_THREADS; i++)
+            if (FINISHED[i] == 0) flag = 0;
+        if (flag == 1)
+            break;
+    }
 
     end = times(NULL);
+    free(threads);
+    free(ints);
 
-    printf("%ld\n", (end-start)/(sysconf(_SC_CLK_TCK)));
+    printf("%lf\n", (end-start)/(1e-6*sysconf(_SC_CLK_TCK)));
+}
+
+
+void writeToFile(char* fileName)
+{
+    FILE *output = fopen(fileName, "w");
+
+    fprintf(output, "P2\n");
+    fprintf(output, "%d %d\n",WIDTH, HEIGHT);
+    fprintf(output, "255\n");
+
+    for (int i=0; i<HEIGHT; i++)
+    {
+        for (int j=0; j<WIDTH; j++)
+        {
+            fprintf(output, "%d ", OUTPUT[i][j]);
+        }
+        fprintf(output, "\n");
+    }
 }
 
 
@@ -167,36 +186,21 @@ int main(int argc, char** argv)
     char* outputImage = argv[4];
 
     INPUT = parseImage(inputImage);
+
     FILTER = parseFilter(filterFile);
 
-    lifeOfThread();
+    FINISHED = calloc(NUM_OF_THREADS, sizeof(int));
 
-    FILE *output = fopen(outputImage, "w");
-    fwrite("P2\n", sizeof(char), 4, output);
+    for (int i=0; i<NUM_OF_THREADS; i++)
+        FINISHED[i] = 0;
 
-    char* height = calloc(10, sizeof(char));
-    char* width = calloc(10, sizeof(char));
-    sprintf(height, "%d", HEIGHT);
-    sprintf(width, "%d", WIDTH);
-    strcat(width, " ");
-    strcat(width, height);
-    strcat(width, "\n");
-    fwrite(width, sizeof(char), 10, output);
+    doSomeWork();
 
-    fwrite("15\n", sizeof(char), 5, output);
+    writeToFile(outputImage);
 
-    char* buf = calloc(10, sizeof(char));
-    for (int i=0; i<HEIGHT; i++)
-    {
-        for (int j=0; j<WIDTH; j++)
-        {
-            sprintf(buf, "%d", OUTPUT[i][j]);
-            strcat(buf, " ");
-            fwrite(buf, sizeof(char), sizeof(buf), output);
-        }
-        fwrite("\n", sizeof(char), 2, output);
-    }
-
+    free(INPUT);
+    free(OUTPUT);
+    free(FILTER);
 
     return 0;
 }
