fefeba6dacd13a6402b05f5202e826b3643991be
Kacpro <kacperj97@gmail.com>
2018-06-09 09:35:59 +0200

    SysOpy lab10 zad1

--- SysOpy/lab10/src/zad1/client.c
+++ SysOpy/lab10/src/zad1/client.c
@@ -8,28 +8,73 @@
 #include <sys/un.h>
 
 
+int calculate(char* e)
+{
+    int arg1 = atoi(strtok(e, " "));
+    char* op = strtok(NULL, " ");
+    int arg2 = atoi(strtok(NULL, " \n"));
+
+    switch(op[0])
+    {
+        case '+': return arg1 + arg2;
+        case '-': return arg1 - arg2;
+        case '*': return arg1 * arg2;
+        case '/': return arg1 / arg2;
+        default: return -1;
+    }
+}
+
+
 void receive(int fd, char* name)
 {
     char recvBuff[1024];
-    memset(recvBuff, '0',sizeof(recvBuff));
 
-    char* msg = calloc(100, sizeof(char));
+    char* msg = calloc(128, sizeof(char));
     strcat(msg, "1");
     strcat(msg, name);
     write(fd, msg, strlen(msg));
+    free(msg);
 
     read(fd, recvBuff, sizeof(recvBuff));
-    printf("aaa%c\n", recvBuff[1]);
 
-    if (recvBuff[0] == '1' && recvBuff[1] == 'N')
+    if (recvBuff[0] == '1' && (recvBuff[1] == 'N' || recvBuff[1] == 'Y'))
     {
-        printf("Given name already exists\n");
+        if (recvBuff[1] == 'N')
+        {
+            printf("Given name already exists in cluster\n");
+            return;
+        }
+        else
+        {
+            printf("Connected to cluster\n");
+        }
+    }
+    else
+    {
+        printf("Unknown message\n");
         return;
     }
 
     while (1)
     {
         recv(fd, recvBuff, sizeof(recvBuff), 0);
+        if (recvBuff[0] == '3')
+        {
+            char* result = calloc(10, sizeof(char));
+            int p = (int)recvBuff[1];
+            printf("Calc request: %s", recvBuff + 2 * sizeof(char));
+            sprintf(result, "%d", calculate(recvBuff + 2 * sizeof(char)));
+
+            char* msg = calloc(128, sizeof(char));
+            char pos[] = {(char)p, '\0'};
+            strcpy(msg, "3");
+            strcat(msg, pos);
+            strcat(msg, result);
+
+            write(fd, msg, 128);
+            free(msg);
+        }
+
         usleep(1000);
     }
 }
@@ -43,7 +88,6 @@ void initNet(char* address, char* name)
     struct sockaddr_in serv_addr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
@@ -61,7 +105,6 @@ void initLocal(char* path, char* name)
     struct sockaddr_un serv_addr;
 
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    memset(&serv_addr, '0', sizeof(serv_addr));
 
     strcpy(serv_addr.sun_path, path);
     serv_addr.sun_family = AF_UNIX;
--- SysOpy/lab10/src/zad1/server.c
+++ SysOpy/lab10/src/zad1/server.c
@@ -1,4 +1,3 @@
-
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/in.h>
@@ -7,31 +6,38 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/epoll.h>
-#include <poll.h>
 #include <pthread.h>
+#include <signal.h>
 
 
-char** clients;
-int* clientfd;
+struct node
+{
+    char* name;
+    int fd;
+    int num;
+};
+
+
 int numOfClients = 0;
 int maxEvenets = 100;
 int clusterSize = 20;
+int socPoll;
+
+
+struct node* clients;
 
 
 int initLocal(char* path)
 {
-    perror("local");
-
     int listenfd = 0;
     struct sockaddr_un serv_addr;
 
     listenfd = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
     serv_addr.sun_family = AF_UNIX;
     strcpy(serv_addr.sun_path, path);
 
+    unlink(path);
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     listen(listenfd, 10);
 
@@ -48,14 +54,11 @@ int initNet(int port)
     int option = 1;
     setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
-    memset(&serv_addr, '0', sizeof(serv_addr));
-
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
 
     bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-
     listen(listenfd, 10);
 
     return listenfd;
@@ -72,128 +75,176 @@ struct soc
 void* monitor(void* arg) {
 
     struct soc soc = *(struct soc*)arg;
+    socPoll = epoll_create1(0);
 
     struct epoll_event event;
-    int poll = epoll_create1(0);
-
-    perror("1");
 
     int netFd = initNet(soc.port);
-
-    perror("2");
-
     event.data.fd = netFd;
     event.events = EPOLLIN | EPOLLET;
-    epoll_ctl(poll, EPOLL_CTL_ADD, netFd, &event);
-
-    perror("a");
+    epoll_ctl(socPoll, EPOLL_CTL_ADD, netFd, &event);
 
     int localFd = initLocal(soc.path);
     event.data.fd = localFd;
     event.events = EPOLLIN | EPOLLET;
-    epoll_ctl(poll, EPOLL_CTL_ADD, localFd, &event);
+    epoll_ctl(socPoll, EPOLL_CTL_ADD, localFd, &event);
 
     struct epoll_event *events = calloc(maxEvenets, sizeof(struct epoll_event));
 
-    char sendBuff[1025];
     char readBuff[128];
 
-    memset(sendBuff, '0', sizeof(sendBuff));
-    memset(readBuff, '0', sizeof(readBuff));
-
-
-    while (1) {
-        int n = epoll_wait(poll, events, maxEvenets, -1);
-        for (int i = 0; i < n; i++) {
-            if ((events[i].events & EPOLLERR) ||
-                (events[i].events & EPOLLHUP) ||
-                (!(events[i].events & EPOLLIN)))
-            {
-                fprintf(stderr, "epoll error\n");
-                close(events[i].data.fd);
-                continue;
-            }
+    while (1)
+    {
+        int n = epoll_wait(socPoll, events, maxEvenets, -1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j=0; j<128; j++) readBuff[j] = '\0';
 
             if (events[i].data.fd == netFd || events[i].data.fd == localFd)
             {
+                int clientFD = accept(events[i].data.fd, (struct sockaddr *) NULL, 0);
+                if (clientFD == -1) continue;
 
-                perror(NULL);
-                int connfd = accept(events[i].data.fd, (struct sockaddr *) NULL, NULL);
-                if (connfd == -1) continue;
-
-
-                recv(connfd, readBuff, strlen(readBuff), 0);
+                recv(clientFD, readBuff, 128, 0);
                 if (readBuff[0] == '1')
                 {
-                    char *read2 = calloc(1024, sizeof(char));
+                    char *read2 = calloc(128, sizeof(char));
                     strcpy(read2, readBuff);
 
-                    char *name = strtok(read2 + sizeof(char), "0");
-                    printf("%s %d\n", name, numOfClients);
-                    perror("if");
+                    char *name = strtok(read2 + sizeof(char), "\0");
+
                     int flag = 1;
-                    for (int i = 0; i < clusterSize; i++)
+                    for (int j = 0; j < clusterSize; j++)
                     {
-                        if (!strcmp(clients[i], name))
+                        if (!strcmp(clients[j].name, name))
                         {
                             flag = 0;
                         }
                     }
+
                     if (flag == 1)
                     {
-                        for (int i=0; i<clusterSize; i++)
-                            if (clientfd[i] == -1)
+                        for (int j=0; j<clusterSize; j++)
+                        {
+                            if (clients[j].fd == -1)
                             {
-                                clients[i] = name;
-                                clientfd[i] = connfd;
+                                clients[j].name = name;
+                                clients[j].fd = clientFD;
+                                clients[j].num = numOfClients;
                                 break;
                             }
+                        }
+                        printf("Node up: %d (%s)\n", numOfClients, name);
                         numOfClients++;
-                        write(connfd, "1Y", 2);
+
+                        event.data.fd = clientFD;
+                        event.events = EPOLLIN | EPOLLET;
+                        epoll_ctl(socPoll, EPOLL_CTL_ADD, clientFD, &event);
+
+                        send(clientFD, "1Y", 2, MSG_DONTWAIT);
                     }
                     else
                     {
-                        write(connfd, "1N", 2);
-                        close(connfd);
+                        write(clientFD, "1N", 2);
+                        close(clientFD);
                     }
+                }
+                else
+                {
+                    printf("Unknown message");
+                }
 
-                    usleep(1000);
+            }
+            else
+            {
+                recv(events[i].data.fd, readBuff, 128, 0);
+                if (readBuff[0] == '3')
+                {
+                    printf("Result: %s, Node: %d\n", strtok(readBuff + 2 * sizeof(char), "\0"), (int)(readBuff[1]) - 1);
                 }
             }
         }
 
+        usleep(1000);
     }
 }
 
 
 void* ping(void* a)
 {
+    struct epoll_event event;
     while (1)
     {
         sleep(1);
         for (int i = 0; i < clusterSize; i++)
         {
-            if (clientfd[i] != -1 && send(clientfd[i], "2", 2, MSG_NOSIGNAL) == -1)
+            if (clients[i].fd != -1 && send(clients[i].fd, "2", 2, MSG_NOSIGNAL) == -1)
             {
-                printf("Node lost\n");
-                clientfd[i] = -1;
-                clients[i] = "";
+                printf("Node down: %d\n", clients[i].num);
+
+                event.data.fd = clients[i].fd;
+                event.events = EPOLLIN | EPOLLET;
+                epoll_ctl(socPoll, EPOLL_CTL_DEL, clients[i].fd, &event);
+
+                close(clients[i].fd);
+                clients[i].fd = -1;
+                clients[i].name = "";
+                clients[i].num = -1;
             }
         }
     }
 }
 
 
+void* calc(void* a)
+{
+    srand(time(0));
+    char* buf = calloc(128, sizeof(char));
+    size_t size = 127;
+    while(1)
+    {
+        char* msg = calloc(130, sizeof(char));
+        strcat(msg, "3");
+        getline(&buf, &size, stdin);
+
+        int target;
+        int p;
+        while ((target = clients[p = rand()%clusterSize].fd) == -1);
+
+        char pos[] = {(char)(p + 1), '\0'};
+        strcat(msg, pos);
+        strcat(msg, buf);
+
+        write(target, msg, 130);
+        free(msg);
+        usleep(1000);
+    }
+}
+
+
+void signalHandling(int sig)
+{
+    for (int i=0; i<clusterSize; i++)
+    {
+        close(clients[i].fd);
+    }
+    free(clients);
+    printf("\n");
+    exit(0);
+}
+
 
 int main(int argc, char** argv)
 {
-    clients = calloc(clusterSize, sizeof(char*));
-    clientfd = calloc(clusterSize, sizeof(int));
+    clients = calloc(clusterSize, sizeof(struct node));
     for (int i=0; i<clusterSize; i++)
     {
-        clients[i] = "";
-        clientfd[i] = -1;
+        clients[i].name = "";
+        clients[i].fd = -1;
+        clients[i].num = -1;
     }
+
+    signal(SIGINT, signalHandling);
 
     pthread_t watcher, pinger, sender;
 
@@ -203,8 +254,11 @@ int main(int argc, char** argv)
 
     pthread_create(&watcher, NULL, monitor, s);
     pthread_create(&pinger, NULL, ping, NULL);
+    pthread_create(&sender, NULL, calc, NULL);
 
-    while(1);
+    pthread_join(watcher, NULL);
+    pthread_join(pinger, NULL);
+    pthread_join(sender, NULL);
 
     return 0;
 }
